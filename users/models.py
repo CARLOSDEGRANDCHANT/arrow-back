1fe22ddfdaeb22cea3c85c657f2eaf9578532710
Carlos De Grandchant <carlosgrandchant@hotmail.com>
2025-07-15 07:35:05 -0400

    Finish user modeling

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class User(AbstractUser):
     """
-    Model for users.
+    Abstract model for users.
     """
     
     ROLE_ADMIN = 'A'
@@ -17,8 +17,7 @@ class User(AbstractUser):
     ]
     
     phone = models.CharField(max_length=20, blank=True)
-    role = models.CharField(max_length=1, choices=ROLE_CHOICES, default=ROLE_LAWYER)
-    
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES, default=ROLE_AGENT)
     
     class Meta:
         verbose_name = _('usuario')
@@ -26,3 +25,29 @@ class User(AbstractUser):
         
     def __str__(self):
         return f"{self.get_full_name()} ({self.get_role_display()})"
+    
+    
+class Agent(models.Model):
+    """
+    Class that extends the abstract User class towards Agent data.
+    """
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    
+class Admin(models.Model):
+    """
+    Class that extends the abstract User class towards Admin data.
+    """
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    
+class Aseguradora(models.Model):
+    name = models.CharField(max_length=200)
+    contact_email = models.EmailField(blank=True, null=True)
+    contact_phone = models.CharField(max_length=20, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
